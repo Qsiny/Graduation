0eeb166b7e1ab55933e378d1d0e932001adbb92c
Qin <1716439531@qq.com>
2021-12-10 22:54:44 +0800

    整合spring security1

--- src/main/java/com/qsiny/graduation/config/SecurityConfig.java
+++ src/main/java/com/qsiny/graduation/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.qsiny.graduation.config;
 
-import com.qsiny.graduation.service.Impl.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
