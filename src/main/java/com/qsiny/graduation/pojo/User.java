a91a87fff95e8d36d7cbee74a9c538db5f786092
Qin <1716439531@qq.com>
2022-03-14 19:05:52 +0800

    使用mp对dao层做了些更改

--- src/main/java/com/qsiny/graduation/pojo/User.java
+++ src/main/java/com/qsiny/graduation/pojo/User.java
@@ -1,45 +1,93 @@
 package com.qsiny.graduation.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.qsiny.graduation.enums.UserSexEnum;
+import com.qsiny.graduation.enums.UserStatusEnum;
+import com.qsiny.graduation.enums.UserTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Objects;
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author Qin
  * @description: TODO
- * @date 2021/12/4 23:52
+ * @date 2022/3/12 13:44
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
-    private int id;
-    private String name;
+@TableName(value = "sys_user")
+public class User implements Serializable {
+    /**
+     * 主键
+     */
+    @TableId
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 昵称
+     */
+    private String nickName;
+    /**
+     * 密码
+     */
     private String password;
-    private String tel;
+    /**
+     * 账号状态（0正常 1停用）
+     */
+    private UserStatusEnum status;
+    /**
+     * 邮箱
+     */
     private String email;
-    private String cid;
-    private String relName;
-    private String role;
-
-    public User(String name, String password, String tel) {
-        this.name = name;
-        this.password = password;
-        this.tel = tel;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && Objects.equals(name, user.name) && Objects.equals(password, user.password) && Objects.equals(tel, user.tel) && Objects.equals(email, user.email) && Objects.equals(cid, user.cid) && Objects.equals(relName, user.relName) && Objects.equals(role, user.role);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, password, tel, email, cid, relName, role);
-    }
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 工作Id
+     */
+    private String jobId;
+    /**
+     * 手机号
+     */
+    private String phonenumber;
+    /**
+     * 用户性别（0男，1女，2未知）
+     */
+    private UserSexEnum sex;
+    /**
+     * 用户类型（0普通用户，1学生，2老师，3管理员）
+     */
+    private UserTypeEnum userType;
+    /**
+     * 创建人的用户id
+     */
+    private Long createBy;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 更新人
+     */
+    private Long updateBy;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+    /**
+     * 删除标志（0代表未删除，1代表已删除）
+     */
+    @TableLogic
+    private Integer delFlag;
 }
